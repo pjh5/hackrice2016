eb5450df928deac2eada49db97c36c461377f1a0
Lily Wen <lw32@rice.edu>
2016-01-17 03:22:07 -0600

    adding all unity files

---
+++ Assets/PaintBrushScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaintBrushScript : MonoBehaviour {
+
+	public int distance;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 myPos = transform.position;
+		myPos.x = myPos.x % 50;
+		myPos.y = myPos.y % 50;
+		myPos.z %= 50;
+		transform.position = myPos;
+
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		Vector3 pos = ray.GetPoint (distance);
+//		this.transform.position = pos;
+//		print ("Position is " + pos.x + ", " + pos.y + ", " + pos.z);
+	}
+
+
+	public void setPainting(bool b)
+	{
+		print ("setPainting on paintbrush");
+	}
+}
